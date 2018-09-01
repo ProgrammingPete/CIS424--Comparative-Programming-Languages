7760aa377c1f4fe371fcbb95b974e0013ad81c49
Peter Parianos <peter.parianos@yahoo.com>
2018-09-01 18:32:25 -0400

    First Commit

---
+++ Homeworks/Project1/wdcount.py
@@ -0,0 +1,26 @@
+import sys
+
+
+def find_file():
+
+
+def create_dict(): 
+
+
+def main():
+    """
+    Author: Peter Parianos
+    
+    This program will count the number of words in a file given as a command line argument,
+    store each name and the number of occurences in a dict,
+    then print words based on certain conditions:
+        Number of different words (number of dictionary entries)
+        Total number of words in the file (num of dict entries * number of occurences)
+        Each word that has more than 5 characters along with its number of ocurrences (most frequent word should be printed first)
+            - key length greater than 5
+        The longest word in the file (either one if there is two of the same length)
+            - maximum key length
+
+    DUE: Sept. 13th - CIS424s
+    turnin -c cis424s -p proj1 wdcount.py
+    """
