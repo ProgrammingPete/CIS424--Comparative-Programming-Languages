b80a0f93c2be61360ae7c6145676c6ac7611e97d
Peter Parianos <peter.parianos@yahoo.com>
2018-09-01 20:29:14 -0400

    Finished Project 1

--- Homeworks/Project1/wdcount.py
+++ Homeworks/Project1/wdcount.py
@@ -1,11 +1,28 @@
 import sys
+import os
 
+def find_file(file_name):
+    """
+    finds and extracts the raw data from the teext file file_name
+    Returns: A list of all words
+    """
+    data = list()
+    with open(file_name) as file:
+        for row in file:
+            data += row.split(' ')
+    
+    for i, word  in enumerate(data): #removes the new line character
+        if '\n' in word:
+            data[i] = word.strip('\n')
+            
+    #print(data)
+    return data
 
-def find_file():
-
-
-def create_dict(): 
-
+def create_dict(data):
+    import collections    
+    datadict = collections.Counter(data)        
+    #print(datadict)
+    return datadict
 
 def main():
     """
@@ -16,7 +33,7 @@ def main():
     then print words based on certain conditions:
         Number of different words (number of dictionary entries)
         Total number of words in the file (num of dict entries * number of occurences)
-        Each word that has more than 5 characters along with its number of ocurrences (most frequent word should be printed first)
+        Each word that has more than 5 characters along with its number of occurrences (most frequent word should be printed first)
             - key length greater than 5
         The longest word in the file (either one if there is two of the same length)
             - maximum key length
@@ -24,3 +41,30 @@ def main():
     DUE: Sept. 13th - CIS424s
     turnin -c cis424s -p proj1 wdcount.py
     """
+    try:
+        file_name = sys.argv[1]
+    except IndexError:
+        print('No argument found, exiting...')
+        sys.exit
+
+    data = find_file(file_name)
+    datadict = create_dict(data)
+    
+    print("Number of unique words: ", len(datadict))
+    print("Total number of words: ", len(data))
+
+    #The longest word in the file
+    sortedList = sorted(data, key= lambda word: len(word), reverse=True)
+    print("longest word in the file: ", sortedList[0])
+    
+    #Each word that has more than 5 characters along with its number of ocurrences
+    print("These words have more than 5 characters:")
+    for key in datadict.keys():
+        if len(key) > 5:
+            print("word: %s, Number of Occurrences: %s" % (key, datadict.get(key)))
+
+    
+main()
+
+    
+    
