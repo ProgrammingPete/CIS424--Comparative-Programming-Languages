c61db3e4e4a17d93109c85876bf035cff15c030b
Peter Parianos <peter.parianos@yahoo.com>
2018-09-14 13:58:36 -0400

    made more changes to specifications

--- Homeworks/HW2-Question-3/parser.py
+++ Homeworks/HW2-Question-3/parser.py
@@ -1,63 +1,76 @@
-#you need to put comments in your code
-#example in the class
-#this is a top-down recurive parser
+#this is a top-down recursive parser
+#Peter Parianos CIS424
 
-import sys
-file = open(sys.argv[1], "r")
-
-
-wlist  = file.read().split()
-
-mitr = iter(wlist)
-
-#this will lok at the first letter, only one is needed
-lookahead = lexan()
-s()
-
-if lookahead == "": #checks if empty
-        print("pass")
-    else:
-        print("Syntax Erorr")
-        exit()
-
-    
-def lexan(): #lexical analyzer
+def lexan(): #lexical analyzer that basically uses an global iterator 
     global mitr
+
+    #we will test the iteration and if an exception occurs, lexan() will return NULL
     try:
-        return(next(mitr)) # this will advance the element by one
+        return(next(mitr)) # this will advance the element by one, the next item in the list
     except StopIteration:
-        return('')
+        return ''
     
 def match(ch):
     global lookahead
 
     if ch == lookahead:
+            #we will check if the passed character is equal to the current lookaead character
+            #if this is true then iterate to the next letter
         lookahead = lexan()
     else:
         print("Syntax Error")
         exit()
 
-def s():
+def S():
+        #this is a start definition: <S> -> <X> z <Y> | <Y> z <X> 
     global lookahead
-    if lookahead == 'a':
-        A()
-        match('c') # different rules: add C()
-    else if lookahead == 'b':
-        B()
-        match('c')
+    
+    #this make use of recursion embedded in the python language itself
+    if lookahead == 'x':
+        X()
+        match('z')
+        Y()
+    elif lookahead == 'y':
+        Y()
+        match('z')
+        X()
     else:
         print("Syntax Error")
         exit()
-def A():
+        
+def X(): #this is for the grammar definition <X> -> x <X> | x 
     global lookahead
-    match('a') # no matter what we will have this because 
-    if lookahead == 'a':
-        A()
-def B():
+    match('x')  #always make sure first letter is x
+    if lookahead == 'x':
+        X()
+        
+def Y(): #this is for the grammar definition <Y> -> y <Y> | y
     global lookahead
-    match('b')
-    if lookahead == 'b':
-        B()
+    match('y')
+    if lookahead == 'y':
+        Y()
+
+#open the file, read the file, and place in wlist delimited with default char  
+import sys
+file = open(sys.argv[1], "r")
+wlist  = file.read().split()
+
+#we will attach an iterator to this list
+mitr = iter(wlist)
+
+#this will look at the next letter, only one is needed. Tis is to give us hits
+lookahead = lexan()
+
+S()
+
+if lookahead == "": #checks if empty, if true then there are no invalid caracters
+        print("pass")
+else:
+        print("Syntax Error")
+        exit()
+
+    
+
     
 
 
