8b902882e06889a8e97f60a64ddf2410bd369d99
Peter Parianos <peter.parianos@yahoo.com>
2018-09-26 21:01:27 -0400

    Added Project 2

---
+++ Homeworks/Project2/eval.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Input is separated by spaces.
+
+type    id_list
+int   x , y , x ;
+real  f , g ;
+this pertains to the lexan() lexical analyzerq
+
+variables  must be declared beofre being used. Also initialized to 0
+create symbol table mapping 'variable_name': [type, val] (dictionaries)
+
+use type() to return the type. can tell if it is an interger or a float
+"""
+
+
+def decl_list():
+
+
+def stmt_list():
+
+
+def prog():
+    decl_list()
+    stmt_list()
+
+def decl():
+    
+
+def type():
+    
+    if type(3.14) == int:
+        print('yes')
