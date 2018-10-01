c4b6752306b54dccaef33c8150931c66b2e3e682
Peter Parianos <peter.parianos@yahoo.com>
2018-10-01 10:42:22 -0400

    added project description

--- Homeworks/Project2/eval.py
+++ Homeworks/Project2/eval.py
@@ -21,79 +21,170 @@ def lexan():
     except StopIteration:
         return ''
 
-def match(string):
+def match(ch):
     global lookahead
-    if string == lookahead:
+    if ch == lookahead:
         lookahead = lexan()
     else:
         print('Syntax Error')
         exit()
-def id_list(symtab, _type):
-    global lookahead
-    name = lookahead
-    if name == ',':
-        print('Syntax Error: Illegal Name')
-        exit()
-    elif name not in symtab:
-        if _type == 'int':
-            symtab[name] = [int, 0] #dictionary points to a list
-        elif _type == 'real':
-            symtab[name] = [float, 0]
-        match(name)
-        return
-    elif name in symtab:
-        print("Syntax Error: Name already Defined")
-        exit()
         
-def decl(symtab, _type):
+def id_list(_type):
     global lookahead
+    global symtab
+    _id = lookahead
+    if _id not in symtab:
+        if _type == 'int':
+            symtab[_id] = [int,0]
+        elif _type == 'real': #reals stored as float
+            symtab[_id] = [float,0]
+        match(_id)
+    if lookahead == ',':
+        match(',')
+        if isId(lookahead):
+            id_list(_type) #recursion
     
-    while True: #user needs to remember to put the ';', else loop with be inf                                          
-        id_list(symtab, _type)
-        if lookahead == ',':
-            match(',')
-            continue
-        elif lookahead == ';':
-            match(';')
-            return
-        else:
-            print("Syntax Error: forgot comma? ")
-            exit()
-                
+def decl():
+    global lookahead
+    _type = lookahead
+    match(_type)
+    if isId(lookahead): 
+        id_list(_type)
+    match(';')
+    
 def decl_list():
-        """Creation of the dictionary (Symbol Table := 'name': [type, val])"""
-        global lookahead        
-        symtab = dict()
-        while True:
-            if lookahead == 'int':
-                match('int')
-                decl(symtab, 'int')
-            elif lookahead == 'real':
-                match('real')
-                decl(symtab, 'real')
-            else:
-                return symtab #this is all variable declarations
+    global lookahead
+    decl()
+    while isType(lookahead):
+        decl()
+        
+def oprnd(_oprnd):
+    global symtab
+    if _oprnd in symtab.keys():
+        match(_oprnd)
+        return symtab[_oprnd][1] # value of id
+    elif isNumber(_oprnd):
+        match(_oprnd)
+        return _oprnd
+    else:
+        print('Syntax Error: invalid operand')
+        exit()
 
+def cond():
+    global lookahead
+    op1 = oprnd(lookahead)
+    if lookahead == '<':
+        match('<')
+        op2 = oprnd(lookahead)
+        return op1 < op2
+    elif lookahead == '>':
+        match('>')
+        op2 = oprnd(lookahead)
+        return op1 > op2
+    elif lookahead == '<=':
+        match('<=')
+        op2 = oprnd(lookahead)
+        return op1 <= op2
+    elif lookahead == '>=':
+        match('>=')
+        op2 = oprnd(lookahead)
+        return op1 >= op2
+    elif lookahead == '==':
+        match('==')
+        op2 = oprnd(lookahead)
+        return op1 == op2
+    elif lookahead == '!=':
+        match('!=')
+        op2 = oprnd(lookahead)
+        return op1 != op2
+    
+def stmt():
+    global lookahead
+    global symtab
+    if isId(lookahead):
+        _id = lookahead
+        if _id not in symtab.keys():
+            print('Syntax Error: invalid Id')
+            exit()
+        else:
+            _type = symtab[_id][0]
+            _cond = bool
+            match(_id)
+            match('=')
+            symtab[_id][1] = expr(_type)
+            if lookahead == 'if':
+                match('if')
+                _cond = cond()
+                match('else')
+                if _cond == False:
+                    symtab[_id][1] = expr(_type)
+                else:
+                    expr(_type)                    
+                match(';')
+            else:
+                match(';')         
+    elif lookahead == 'printi':
+        match('printi')
+        print(expr(int))
+        match(';')
+    elif lookahead == 'printr':
+        match('printr')
+        print(expr(float))
+        match(';')
+    else:
+        print('Syntax Error: Invalid Symbol')
+        exit()
+
+def stmt_list():
+    global lookahead
+    stmt()
+    while isId(lookahead) or lookahead == 'printf' or lookahead == 'printi':
+        stmt()
+        
 def prog():
     global lookahead
     while True:
-        if lookahead == 'int' or lookahead == 'real':
-             symtab = decl_list()
+        if isType(lookahead):
+            decl_list()
         else:
             stmt_list()
+        if lookahead == '':
+            print('Pass')
+            break
 
+def isType(ch):
+    types = ['int', 'real']
+    if ch not in types:
+        return False
+    return True
+
+def getType(ch):
+    return type(ch)
+
+def isId(ch):
+    global reserved_words
+    if isNumber(ch):
+        return False
+    elif ch in reserved_words:
+        return False
+    return True
+
+def isNumber(ch):
+    try:
+        float(ch)
+        return True
+    except ValueError:
+        return False
+
+    
 #start of the program
 import sys
 file = open(sys.argv[1], "r")
 wlist = file.read().split()
 
+symtab = dict()
+reserved_words = ['printi', 'printr','int', 'real',',', ';','(',')','^','/', '=', '+', '-', '*', '<', '>', '<=', '>=', '==', '!=', 'if', 'else'] 
 mitr = iter(wlist)
 lookahead = lexan()
 
-prog()
-
-if lookahead == "":
-    print("pass")
-else:
-    print("Syntax Error")
-    exit()
+prog() 
