368f653fea34cffe8a91497ac34fb173719c49d1
Peter Parianos <peter.parianos@yahoo.com>
2018-11-07 22:09:51 -0500

    added project 4

---
+++ Homeworks/Project4/prog4.pl
@@ -0,0 +1,30 @@
+
+male(mushu).
+male(tangdee).
+female(mulan).
+female(beumei).
+female(gugu).
+
+father(baba,mushu).
+father(baba,mulan).
+father(yeye,baba).
+father(yeye,gugu).
+father(yeye,susu).
+father(susu,tangdee).
+father(zengzufu,yeye).
+father(jojo,beumei).
+
+mother(mama,mushu).
+mother(mama,mulan).
+mother(popo,mama).
+mother(popo,jojo).
+
+%addedrules
+parent(X,Y) :- mother(X,Y).
+parent(X,Y) :- father(X,Y).
+
+sibling(X,Y):- parent(Z,X), parent(Z,Y), not(X = Y).
+brother(X,Y):- sibling(X,Y), male(X).
+aunt(X,Y):- parent(Z,Y), sibling(Z,X), female(X).
+granddaughter(X,Y):- female(X), ! , parent(Z,X), parent(Y,Z).
+descendant(X,Y) :- parent(Y,X).
